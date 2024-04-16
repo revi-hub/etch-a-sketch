830b70125cbe9f4e47aa3be6d04d0714e3b4ac4b
Revi <150064623+revi-hub@users.noreply.github.com>
2024-04-16 21:00:05 +0200

    Add change grid button

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const pixelsContainer = document.getElementById("pixels-container");
+const btn = document.querySelector("button");
+let pixels;
 
 function createGrid(size){
     if(size > 0 && size <= 100){
@@ -10,10 +12,11 @@ function createGrid(size){
         for(let i = 0; i < size; i++) {
             pixels.push(document.createElement("div"));
             pixels[i].style.width = width;
+            pixels[i].classList.add("pixel");
             pixelsContainer.appendChild(pixels[i]);
         }
         console.log('SUCCESS: Built a new grid');
-        return pixels;
+        return pixels.length;
     } else {
         console.log('ERROR: Specified invalid number');
         return null;
@@ -21,4 +24,17 @@ function createGrid(size){
 }
 
 createGrid(16);
+
+
+pixelsContainer.addEventListener("mouseover", (e) => {
+    if(e.target.classList.contains("pixel"))
+    e.target.classList.add("drawn");
+});
+
+btn.addEventListener("click", () => {
+
+    const numberOfPixels = prompt("Please specify number of pixels per side: ");
+    pixels = createGrid(numberOfPixels);
+});
+
 
