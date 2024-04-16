96af10863e3add127014fcecdafe9303cf206fdb
Revi <150064623+revi-hub@users.noreply.github.com>
2024-04-16 20:26:00 +0200

    Rewrite creating a grid in to a function

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const pixelsContainer = document.getElementById("pixels-container");
-let pixels = [];
 
-for(var i = 0; i < 256; i++) {
-    pixels.push(document.createElement("div"));
-    pixelsContainer.appendChild(pixels[i]);
+function createGrid(size){
+    if(size > 0 && size <= 100){
+        let pixels = [];
+        let width = 1 / size * 100;
+        width = width.toFixed(3);
+        width = width.toString() + '%'
+        size = size * size;
+        for(let i = 0; i < size; i++) {
+            pixels.push(document.createElement("div"));
+            pixels[i].style.width = width;
+            pixelsContainer.appendChild(pixels[i]);
+        }
+        console.log('SUCCESS: Built a new grid');
+        return pixels;
+    } else {
+        console.log('ERROR: Specified invalid number');
+        return null;
+    }   
 }
 
-pixelsContainer.addEventListener("mouseover", (e) => {
-    e.target.classList.add("drawn")
-});
+createGrid(16);
+
