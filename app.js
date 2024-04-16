8dd425a23884aa55ebfab6f038765a2ab601aa04
Revi <150064623+revi-hub@users.noreply.github.com>
2024-04-16 19:35:30 +0200

    Implement drawing on the pixels grid

--- app.js
+++ app.js
@@ -5,3 +5,7 @@ for(var i = 0; i < 256; i++) {
     pixels.push(document.createElement("div"));
     pixelsContainer.appendChild(pixels[i]);
 }
+
+pixelsContainer.addEventListener("mouseover", (e) => {
+    e.target.classList.add("drawn")
+});
